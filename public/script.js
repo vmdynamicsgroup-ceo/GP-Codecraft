456b7404cb5725033c22ca80061fd2de758cc5be
vmdynamicsgroup-ceo <vmdynamicsgroup@gmail.com>
2026-08-28 16:17:57 +0530

    Implement Google sign-in functionality
    Add onSignIn function to handle Google user sign-in.

--- public/script.js
+++ public/script.js
@@ -487,3 +487,10 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+}
